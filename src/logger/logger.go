f445d0f5d600dd6fdc2fa939d7d02c46a1d99341
Гиевой Виктор <gievoi.v@gmail.com>
2018-05-10 16:48:41 +0300

    fixes

--- src/logger/logger.go
+++ src/logger/logger.go
@@ -7,23 +7,26 @@ import (
 )
 
 var (
-	info           *log.Logger
-	warning        *log.Logger
-	error          *log.Logger
+	info    *log.Logger
+	warning *log.Logger
+	erro   *log.Logger
 )
 
 func init() {
-	logFile, err := os.OpenFile(os.Getenv("LOG_PATH"), os.O_CREATE | os.O_APPEND | os.O_RDWR, 0666)
-	if err != nil {
-		panic(err)
+	logFile, err := os.OpenFile(os.Getenv("LOG_PATH"), os.O_CREATE|os.O_APPEND|os.O_RDWR, 0666)
+	if err == nil {
+		info = log.New(os.Stdout, "INFO: ", log.Ldate|log.Ltime|log.Lshortfile)
+		warning = log.New(os.Stdout, "WARNING: ", log.Ldate|log.Ltime|log.Lshortfile)
+		erro = log.New(os.Stderr, "ERROR: ", log.Ldate|log.Ltime|log.Lshortfile)
+	} else {
+		info = log.New(io.MultiWriter(os.Stdout, logFile), "INFO: ", log.Ldate|log.Ltime)
+		warning = log.New(io.MultiWriter(os.Stdout, logFile), "WARNING: ", log.Ldate|log.Ltime)
+		erro = log.New(io.MultiWriter(os.Stdout, logFile), "ERROR: ", log.Ldate|log.Ltime)
 	}
 
-	info = log.New(io.MultiWriter(os.Stdout, logFile), "INFO: ", log.Ldate|log.Ltime)
-	warning = log.New(io.MultiWriter(os.Stdout, logFile), "WARNING: ", log.Ldate|log.Ltime)
-	error = log.New(io.MultiWriter(os.Stdout, logFile), "ERROR: ", log.Ldate|log.Ltime)
 }
 
-func Info(v ...interface{})  {
+func Info(v ...interface{}) {
 	info.Println(v)
 }
 
@@ -31,6 +34,6 @@ func Warning(v ...interface{}) {
 	warning.Println(v)
 }
 
-func Error(v ...interface{})  {
-	error.Println(v)
+func Error(v ...interface{}) {
+	erro.Println(v)
 }
